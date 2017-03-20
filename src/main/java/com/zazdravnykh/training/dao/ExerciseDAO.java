74254608dd2f1d19336c122ffe62c62e790624f5
Yevhen Zazdravnykh <yevhen.zazdravnykh@agiliway.com>
2017-03-20 12:34:55 +0200

    test commit from another pc

--- src/main/java/com/zazdravnykh/training/dao/ExerciseDAO.java
+++ src/main/java/com/zazdravnykh/training/dao/ExerciseDAO.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zazdravnykh.training.entity.Exercise;
 
+//just a test comment
 public interface ExerciseDAO extends JpaRepository<Exercise, Integer> {
 
 }
